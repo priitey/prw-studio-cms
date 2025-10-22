28f59f17266bec664f8a5e2b6e8c29420aeda9ca
priitey <111400977+priitey@users.noreply.github.com>
2025-10-22 16:15:24 +1100

    updated cors in config file;

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -22,7 +22,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  // cors: ['https://prw.studio'], // uncomment this so only prw.studio can call this app's API
+  cors: ['https://prw.studio', 'http://127.0.0.1:5500'], // uncomment this so only prw.studio can call this app's API
   collections: [Users, Media, Projects, Tags],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
