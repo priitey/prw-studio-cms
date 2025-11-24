1c1fbb7eae8dcb8c4eb214fae0f9c59c10bc1d16
priitey <111400977+priitey@users.noreply.github.com>
2025-11-24 13:00:31 +1100

    deleted unused line; added attributes to metadata const;

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,6 +4,13 @@ import './styles.css'
 export const metadata = {
   description: 'The CMS app for the website of prw.studio. Design, Coding and Multimedia projects are categorised, uploaded and edited via this app.',
   title: 'prw.studio CMS',
+  keywords: ['CMS', 'prw.studio', 'design', 'multimedia'],
+  authors: [{ name: 'Pritey', url: 'https://prw.studio' }],
+  creator: 'Pritey',
+  publisher: 'prw.studio',
+  themeColor: '#c7c7c7',
+  locale: 'en_US',
+  type: 'website'
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
